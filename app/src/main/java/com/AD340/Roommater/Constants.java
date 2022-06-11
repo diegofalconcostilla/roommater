0ef31c8c97b0bb8f7d099bc00dd760d92405acef
Namuna Kassaye <your_email_address@example.com>
2022-06-11 10:37:17 -0700

    adding Constants

---
+++ app/src/main/java/com/AD340/Roommater/Constants.java
@@ -0,0 +1,9 @@
+package com.AD340.Roommater;
+
+public class Constants{
+    public static final String USER_NAME_KEY = "username";
+    public static final String USER_AGE_KEY = "age";
+    public static final String USER_EMAIL_KEY = "email";
+    public static final String USER_ZIP_KEY = "location";
+    public static final String USER_PASSWORD_KEY = "password";
+}
